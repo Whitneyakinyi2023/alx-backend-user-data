be9bf197919ec7a46f28b3483680264716b19bbc
Whitneyakinyi2023 <whitneyakinyi73@gmail.com>
2024-08-30 01:16:21 +0300

    Implemented password hashing and validation using bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -31,6 +31,7 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
         password (str): The password to validate.
 
     Returns:
-        bool: True if the password matches the hashed password, False otherwise.
+        bool: True if the password matches
+        the hashed password, False otherwise.
     """
     return bcrypt.checkpw(password.encode(), hashed_password)
